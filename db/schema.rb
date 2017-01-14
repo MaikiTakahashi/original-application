16edcaea6c276af035816d57e8b7d1d636849251
Maiki <m1223takahashi@gmail.com>
2017-01-14 04:00:49 +0000

    いったんオススメスタッフを1名rubyで表示させる

--- db/schema.rb
+++ db/schema.rb
@@ -14,22 +14,12 @@
 ActiveRecord::Schema.define(version: 20161230232803) do
 
   create_table "maids", force: :cascade do |t|
-    t.string   "name"
-    t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "time"
-    t.string   "place"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "title"
-    t.string   "description"
-    t.string   "address"
-    t.float    "latitude"
-    t.float    "longitude"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "name",        limit: 255
+    t.string   "description", limit: 255
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.string   "time",        limit: 255
+    t.string   "place",       limit: 255
   end
 
 end
