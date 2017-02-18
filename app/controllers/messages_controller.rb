217e148d4c6e3e6d4fbf3fd0203f799d11fb2786
Maiki Takahashi <m1223takahashi@gmail.com>
2017-02-18 00:03:33 +0000

    エラー表示メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,14 @@
 class MessagesController < ApplicationController
   def create
-    @message = Maid.find(params[:message][:id]).messages.build(message_params)
+    @message = Maid.find(params[:message][:maid_id]).messages.build(message_params)
     if @message.save
-      redirect_to maid_path(params[:message][:id]) , notice: 'メッセージを保存しました'
+      redirect_to maid_path(params[:message][:maid_id]) , notice: 'メッセージを保存しました'
     else
       flash.now[:alert] = "メッセージの保存に失敗しました"
-      redirect_to maid_path(params[:message][:id])
+      #redirect_to maid_path(params[:message][:id])
+      @maid = Maid.find(params[:message][:maid_id])
+      @messages = Message.all
+      render 'maids/show'
     end
   end
   
